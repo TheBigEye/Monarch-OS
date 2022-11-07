a5779d2fbe2d64eec38c6f237bfea55386f8a9d1
TheBigEye <thebigeyedev@gmail.com>
2022-11-07 02:51:04 -0300

    Keyboard Handler hot fix!

--- src/drivers/keyboard.cpp
+++ src/drivers/keyboard.cpp
@@ -8,25 +8,42 @@ bool LeftShiftPressed = false;
 bool RightShiftPressed = false;
 uint_8 LastScancode;
 
+// We use a buffer for avoid delete the printed text before the KeyboardHandler initialization
+char KeyboardBuffer[256];
+uint_8 KeyboardBufferIndex = 0;
+
 void StandardKeyboardHandler(uint_8 scanCode, uint_8 chr) {
     if (chr != 0) {
         switch (LeftShiftPressed | RightShiftPressed) {
-            case true: display::print_char(chr - 32); break;
-            case false: display::print_char(chr); break;
+            case true:
+                display::print_char(chr - 32);
+                KeyboardBuffer[KeyboardBufferIndex++] = chr - 32;
+                break;
+            case false:
+                display::print_char(chr);
+                KeyboardBuffer[KeyboardBufferIndex++] = chr;
+                break;
         }
     } else {
         switch (scanCode) {
             case 0x8E: // Backspace.
-                display::set_cursor_pos(get_cursor_pos() - 1);
-                display::print_char(' ');
-                display::set_cursor_pos(get_cursor_pos() - 1);
+                if (KeyboardBufferIndex > 0) {
+                    display::set_cursor_pos(get_cursor_pos() - 1);
+                    display::print_char(' ');
+                    display::set_cursor_pos(get_cursor_pos() - 1);
+                    KeyboardBuffer[--KeyboardBufferIndex] = 0;
+                }
                 break;
 
             case 0x2A: LeftShiftPressed = true; break; // Left shift
             case 0xAA: LeftShiftPressed = false; break; // Left shift released
             case 0x36: RightShiftPressed = true; break; // Right shift
             case 0xB6: RightShiftPressed = false; break; // Right shift released
-            case 0x9C: display::print_string("\n\r"); break; // Enter
+            case 0x9C:
+                display::print_string("\n\r");
+                KeyboardBuffer[KeyboardBufferIndex] = 0;
+                KeyboardBufferIndex = 0;
+                break; // Enter
         }
     }
 }
